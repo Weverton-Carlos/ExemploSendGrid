6d93342f08decdc4a7eb863d81daabb0e395e5e8
Weverton Leal <weverton_carlos@outlook.com>
2020-06-01 14:14:18 -0300

    alteração iii

--- SendGridExemplo.cs
+++ SendGridExemplo.cs
@@ -21,13 +21,17 @@ namespace ExemploSendGrid
             var apiKey = Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
             //cliente
             var client = new SendGridClient(apiKey);
+            
+            //mensagem
             var msg = new SendGridMessage()
             {
+                //remetente
                 From = new EmailAddress("test@example.com", "DX Team"),
                 Subject = "Hello World from the SendGrid CSharp SDK!",
                 PlainTextContent = "Hello, Email!",
                 HtmlContent = "<strong>Hello, Email!</strong>"
             };
+            //add destinatario
             msg.AddTo(new EmailAddress("test@example.com", "Test User"));
             var response = await client.SendEmailAsync(msg);
         }
